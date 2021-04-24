9985f9264ff3a6b63b44e59bfb9704814b64cacb
Vishal <vishalbhagwat92@gmail.com>
2021-04-24 14:08:41 +0530

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule} from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatTableModule} from '@angular/material/table';
-import { MatTabsModule} from '@angular/material/tabs';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -43,20 +37,14 @@ import { TooltipDirectiveDirective } from './directives/tooltip-directive.direct
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    MatSelectModule,
     MatFormFieldModule,
     MatInputModule,
-    MatTableModule,
-    MatTabsModule,
     MatButtonModule,
-    MatCardModule,
     MatDialogModule,
-    MatGridListModule,
     MatMenuModule,
     MatTooltipModule,
     FormsModule, ReactiveFormsModule,
